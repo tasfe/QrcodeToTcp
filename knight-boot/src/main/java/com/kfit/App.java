b2ad3e95266c085e749bf11cdadc439442609737
zhangjin <shanghaivip1@gmail.com>
2017-05-10 16:53:40 +0800

    删除无用的类

--- knight-boot/src/main/java/com/kfit/App.java
+++ knight-boot/src/main/java/com/kfit/App.java
@@ -6,10 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 
@@ -48,6 +54,33 @@ public class App {
 		qrcodeTcpMessageSender.openDoor("0090C2501025","123456789");
 		return "open success";
 	}
+
+	//这里指定是条状的jsp界面
+	@RequestMapping(value = "/index1")
+	public String index(Model model) {
+		model.addAttribute("sb", "this is my fries测试不是好领导了副经理看fjldj 1123123");
+		return "index";
+	}
+
+	@RequestMapping(value = "json")
+	@ResponseBody
+	public Map<String, Object> mytest() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("name", "Ryan");
+		map.put("age", "3");
+		map.put("sex", "man");
+		return map;
+	}
+
+
+	@Bean
+	InternalResourceViewResolver internalResourceViewResolver () {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
